036e2fb2c911fac0bdbe6db6059b90fe348249fd
Yuvaraj360 <137602148+Yuvaraj360@users.noreply.github.com>
2023-07-17 12:25:30 +0530

    Add files via upload

---
+++ day 7/Main (1).java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.io*;
+import java.util.*;
+class UserMainCode
+{
+   public int allDigitsCount(int input1)
+{
+   String str=Integer.toString(input1);
+   return str.length();
+  }
+}
